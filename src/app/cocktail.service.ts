2dc0f6fbec2b752e1aedc03b6e59984a07edab7e
Sharan Raj <52441714+ssharanraj@users.noreply.github.com>
2022-03-02 19:49:19 +0530

    Add files via upload
    add app

---
+++ src/app/cocktail.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CocktailResponse } from './cocktail.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CocktailService {
+
+  constructor(private http: HttpClient) { }
+
+  getRecipies(searchTerm:string) : Observable<CocktailResponse> {
+    return this.http.get<CocktailResponse>(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchTerm}`)
+  }
+
+  getDetails(searchTerm:string) : Observable<CocktailResponse> {
+    return this.http.get<CocktailResponse>(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${searchTerm}`)
+  }
+}
